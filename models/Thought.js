078a69ed70aa2f28a462524b67ab351361708614
Cha Vue <chavue09@gmail.com>
2022-05-23 16:32:41 -0500

    models created

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+// Importing Dependencies 
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false   
+}
+); 
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
